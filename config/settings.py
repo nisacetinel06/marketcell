b2cf88763a0ce0b294d1105a6000ade24e4e075a
Abdulsametklc <sametklgs@gmail.com>
2026-05-14 23:48:13 +0300

    Add drf-spectacular for Swagger and full-text search signals

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'core',
+    'drf_spectacular',
     'corsheaders',
 ]
 
@@ -115,6 +116,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 # Internationalization
